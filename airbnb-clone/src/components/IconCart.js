a242b198123b4fe90a166f4f0da8fc787a35422d
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-05-13 00:46:46 +0700

    sửa đổi icon trên menu bar thành component icon cart

---
+++ airbnb-clone/src/components/IconCart.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const IconCart = () => {
+  return (
+    <>
+      <span>
+        <i className="fas fa-shopping-cart"></i>
+      </span>
+      <span className="badge rounded-pill badge-notification bg-danger">1</span>
+    </>
+  );
+};
