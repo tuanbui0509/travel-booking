6c6e3bcabb2593cd8f4375038ff999ca6c5a8576
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-05-13 00:43:24 +0700

    Sửa đổi trang giỏ hàng

--- airbnb-clone/src/components/CartItem.js
+++ airbnb-clone/src/components/CartItem.js
@@ -1,16 +1,34 @@
 import "../styles/Cart.css";
 export const CartItem = ({ item, handleDelete }) => {
   return (
-    <div className="d-flex justify-content-between align-items-center mt-3 p-2 items rounded">
+    <div className="d-flex justify-content-between align-items-center mt-3 p-2 items rounded flex-wrap">
       <div className="d-flex flex-row">
         <img className="rounded image-tour-cart" src={item.image} width="40" />
         <div className="ml-2 brief">
           <span className="font-weight-bold d-block">{item.name}</span>
-          <span className="spec">{item.duration}</span>
+          <ul>
+            <li>
+              <span className="spec">
+                <i class="far fa-clock item-icon"></i>Thời gian: {item.duration}
+              </span>
+            </li>
+            <li>
+              <span className="spec">
+                <i class="far fa-calendar-alt item-icon"></i>Ngày khởi hành:{" "}
+                {item.departure_date}
+              </span>
+            </li>
+            <li>
+              <span className="spec">
+                <i class="far fa-user item-icon"></i>Số người: {item.quantity}
+              </span>
+            </li>
+            <li></li>
+          </ul>
         </div>
       </div>
+
       <div className="d-flex flex-row align-items-center">
-        <span className="d-block">{item.quantity}</span>
         <span className="d-block ml-5 font-weight-bold">${item.price}</span>
         <button
           className="btn-sm btn-outline-danger ml-3"
--- airbnb-clone/src/pages/Cart.js
+++ airbnb-clone/src/pages/Cart.js
@@ -84,21 +84,11 @@ export const Cart = () => {
             <div className="col-md-8">
               <div className="product-details mr-2">
                 <div className="d-flex flex-row align-items-center">
-                  <i className="fa fa-long-arrow-left"></i>
-                  <span className="ml-2">Continue Shopping</span>
+                  <i class="fas fa-arrow-left"></i>
+                  <span className="ml-2">Tiếp tục thêm</span>
                 </div>
                 <hr />
-                <h6 className="mb-0">Shopping cart</h6>
-                <div className="d-flex justify-content-between">
-                  <span>You have 4 items in your cart</span>
-                  <div className="d-flex flex-row align-items-center">
-                    <span className="text-black-50">Sort by:</span>
-                    <div className="price ml-2">
-                      <span className="mr-1">price</span>
-                      <i className="fa fa-angle-down"></i>
-                    </div>
-                  </div>
-                </div>
+                <h4 className="mb-0 mb-2">Shopping cart</h4>
                 <div className="overflow-auto" style={{ maxHeight: "400px" }}>
                   {cartItems.map((item) => (
                     <CartItem
@@ -111,11 +101,36 @@ export const Cart = () => {
               </div>
             </div>
             <div className="col-md-4">
-              <Checkout />
+              <div className="payment-info">
+                <div className="d-flex justify-content-between information">
+                  <span>Tổng tiền</span>
+                  <span>$3000.00</span>
+                </div>
+                <div className="d-flex justify-content-between information">
+                  <span></span>
+                  <span>$20.00</span>
+                </div>
+                <div className="d-flex justify-content-between information">
+                  <span>Total(Incl. taxes)</span>
+                  <span>$3020.00</span>
+                </div>
+                <Link className="text-reset me-3" to={"/checkout"}>
+                  <button
+                    className="btn btn-primary btn-block d-flex justify-content-between mt-3"
+                    type="button"
+                  >
+                    <span>$3020.00</span>
+                    <span>
+                      Checkout<i className="fa fa-long-arrow-right ml-1"></i>
+                    </span>
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <Footer />
     </>
   );
