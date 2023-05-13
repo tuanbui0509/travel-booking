e103ad73bc8ad07554c64743f7b7c9ccc2a74075
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-05-14 02:21:00 +0700

    Sửa Trang thanh toán

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -11,21 +11,24 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Cart } from "./pages/Cart";
 import { Checkout } from "./pages/Checkout";
 import Tour from "./pages/Tour";
+import {SelectedTourProvider} from "./contexts/SelectedTourContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/productList" element={<ProductList />} />
-        <Route path="/productList/productId:" element={<Product />} />
-        <Route path="/tourList/tourId:" element={<Tour />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </Router>
+      <SelectedTourProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/productList" element={<ProductList />} />
+            <Route path="/productList/productId:" element={<Product />} />
+            <Route path="/tourList/tourId:" element={<Tour />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </Router>
+      </SelectedTourProvider>
   );
 }
 
--- airbnb-clone/src/pages/Checkout.js
+++ airbnb-clone/src/pages/Checkout.js
@@ -1,18 +1,109 @@
-import { Link } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import {useEffect, useState} from "react";
+import {FormInputInfoCustomer} from "../components/checkout/FormInputInfoCustomer";
+import {useSelectedTour} from "../contexts/SelectedTourContext";
+import CardItem from "../components/CardItem";
 
 export const Checkout = () => {
-  return (
-    <>
-      <div className="row flex-wrap">
-        <div className="col-md-6">
-          <div>
-            <input type="text" />
-          </div>
-        </div>
-        <div className="col-md-6"></div>
-      </div>
-    </>
-  );
+    const { selectedTour } = useSelectedTour();
+    const location = useLocation();
+    const [total, setTotal] = useState("0");
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const totalParam = searchParams.get("total");
+        setTotal(totalParam || "0");
+    }, [location.search]);
+
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="row flex-wrap">
+                    <div className="col-md-8 mt-4">
+                        <h3>Thông tin liên hệ</h3>
+                        <div className="row">
+                            <div className="col-md-6 mt-4">
+                                <div className="form-group">
+                                    <label>Tên(<span className="text-danger">*</span>) </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        placeholder="Nhập tên của bạn"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Email(<span className="text-danger">*</span>) </label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="Nhập email của bạn"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Thành phố</label>
+                                    <input type="text" className="form-control" id="city" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 mt-4">
+                                <div className="form-group">
+                                    <label>
+                                        Số điện thoại(<span className="text-danger">*</span>){" "}
+                                    </label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        id="phone"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>
+                                        Địa chỉ(<span className="text-danger">*</span>){" "}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="address"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Quốc gia</label>
+                                    <input type="email" className="form-control" id="email" />
+                                </div>
+                            </div>
+                        </div>
+                        {selectedTour ? (
+                            <FormInputInfoCustomer quantity={selectedTour.quantity} />
+                        ) : (
+                            <h3 className='text-danger'><i className="fas fa-arrow-right ml-1"></i> Không có tour nào để thanh toán</h3>
+                        )}
+                    </div>
+                    <div className="col-md-4 mt-4">
+                        <h3>Tổng tiền </h3>
+                        <div className="row">
+                            <div className="col-md-12 mt-4">
+                                <div className="form-group">
+                                    <label htmlFor="total">Tổng</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="total"
+                                        value={total + "đ"}
+                                        readOnly
+                                    />
+                                </div>
+                                <div className="btn btn-primary rounded">Thanh toán</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 };
+
