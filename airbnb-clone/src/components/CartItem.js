ca9d536a5f23c3c73c4554eaa5c665e34c0c0116
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-05-14 13:44:27 +0700

    Sửa giỏ hàng . chỉ cho phép chọn 1 chuyến trong giỏ hàng để thanh toán

--- airbnb-clone/src/components/CartItem.js
+++ airbnb-clone/src/components/CartItem.js
@@ -1,42 +1,73 @@
 import "../styles/Cart.css";
-export const CartItem = ({ item, handleDelete }) => {
+import { useEffect, useState } from "react";
+
+export const CartItem = ({ item, handleDelete, handleQuantityChange, selectedItemId, handleSelection }) => {
+  const [quantity, setQuantity] = useState(item.quantity);
+  const [selected, setSelected] = useState(item.selected || false);
+  const isSelected = selectedItemId === item.id; // Kiểm tra xem item có được chọn hay không
+
+  useEffect(() => {
+    handleQuantityChange(item.id, quantity);
+  }, [quantity]);
+
+  const handleTick = () => {
+    if (isSelected) {
+      handleSelection(null); // Nếu item đã được chọn, bỏ chọn nó
+    } else {
+      handleSelection(item.id); // Nếu item chưa được chọn, chọn nó
+    }
+  };
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
-    <div className="d-flex justify-content-between align-items-center mt-3 p-2 items rounded flex-wrap">
-      <div className="d-flex flex-row">
-        <img className="rounded image-tour-cart" src={item.image} width="40" />
-        <div className="ml-2 brief">
-          <span className="font-weight-bold d-block">{item.name}</span>
-          <ul>
-            <li>
+      <div className="d-flex justify-content-between align-items-center mt-3 pr-2 items rounded flex-wrap">
+        <div className="d-flex flex-row">
+          <img className="rounded image-tour-cart" src={item.image} width="40" alt={item.name} />
+          <div className="ml-2 brief">
+            <span className="font-weight-bold d-block">{item.name}</span>
+            <ul>
+              <li>
               <span className="spec">
-                <i class="far fa-clock item-icon"></i>Thời gian: {item.time}
+                <i className="far fa-clock item-icon"></i>Thời gian: {item.time}
               </span>
-            </li>
-            <li>
+              </li>
+              <li>
               <span className="spec">
-                <i class="far fa-calendar-alt item-icon"></i>Ngày khởi hành:{" "}
-                {item.date}
+                <i className="far fa-calendar-alt item-icon"></i>Ngày khởi hành: {item.date}
               </span>
-            </li>
-            <li>
+              </li>
+              <li>
               <span className="spec">
-                <i class="far fa-user item-icon"></i>Số người: {item.quantity}
+                <i className="far fa-user item-icon"></i>Số người: {quantity}
               </span>
-            </li>
-            <li></li>
-          </ul>
+                <div className="btn-group ">
+                  <button className="btn btn-sm" onClick={handleDecrease}>-</button>
+                  <span className="btn btn-sm">{quantity}</span>
+                  <button className="btn btn-sm" onClick={handleIncrease}>+</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="d-flex flex-row align-items-center">
+          {isSelected ? (
+              <i className="fas fa-check-circle tick-icon-selected" onClick={handleTick}></i>
+          ) : (
+              <i className="far fa-check-circle tick-icon" onClick={handleTick}></i>
+          )}
+          <span className="d-block font-weight-bold ml-4">{item.price}đ</span>
+          <button className="btn-sm btn-outline-danger ml-3" onClick={() => handleDelete(item.id)}>
+            <i className="fas fa-trash-alt"></i>
+          </button>
         </div>
       </div>
-
-      <div className="d-flex flex-row align-items-center">
-        <span className="d-block ml-5 font-weight-bold">${item.price}</span>
-        <button
-          className="btn-sm btn-outline-danger ml-3"
-          onClick={() => handleDelete(item.id)}
-        >
-          <i className="fas fa-trash-alt"></i>
-        </button>
-      </div>
-    </div>
   );
 };
--- airbnb-clone/src/pages/Cart.js
+++ airbnb-clone/src/pages/Cart.js
@@ -1,137 +1,153 @@
-import { Link } from "react-router-dom";
-import { CartItem } from "../components/CartItem";
+import {Link} from "react-router-dom";
+import {CartItem} from "../components/CartItem";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/Cart.css";
-import { Checkout } from "./Checkout";
-import { useState } from "react";
+import {useState} from "react";
+import {useSelectedTour} from "../contexts/SelectedTourContext";
 
 export const Cart = () => {
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
-      time: "4 ngày 3 đêm",
-      date: "11/06/2023",
-      quantity: 2,
-      price: "5,290,000",
-      image:
-        "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
-    },
-    {
-      id: 2,
-      name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
-      time: "4 ngày 3 đêm",
-      date: "11/06/2023",
-      quantity: 2,
-      price: "5,290,000",
-      image:
-        "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
-    },
-    {
-      id: 3,
-      name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
-      time: "4 ngày 3 đêm",
-      date: "11/06/2023",
-      quantity: 2,
-      price: "5,290,000",
-      image:
-        "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
-    },
-    {
-      id: 4,
-      name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
-      time: "4 ngày 3 đêm",
-      date: "11/06/2023",
-      quantity: 2,
-      price: "5,290,000",
-      image:
-        "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
-    },
-  ]);
+    const [cartItems, setCartItems] = useState([
+        {
+            id: 1,
+            name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
+            time: "4 ngày 3 đêm",
+            date: "11/06/2023",
+            quantity: 2,
+            price: "5,290,000",
+            image:
+                "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
+        },
+        {
+            id: 2,
+            name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
+            time: "4 ngày 3 đêm",
+            date: "11/06/2023",
+            quantity: 2,
+            price: "5,290,000",
+            image:
+                "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
+        },
+        {
+            id: 3,
+            name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
+            time: "4 ngày 3 đêm",
+            date: "11/06/2023",
+            quantity: 2,
+            price: "5,290,000",
+            image:
+                "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
+        },
+        {
+            id: 4,
+            name: "PHÚ QUỐC | THỊ TRẤN HOÀNG HÔN SUNSET TOWN",
+            time: "4 ngày 3 đêm",
+            date: "11/06/2023",
+            quantity: 2,
+            price: "5,290,000",
+            image:
+                "https://globaltravel.com.vn/wp-content/uploads/2020/10/globaltravel-tour-du-lich-gia-re-trong-nuoc-trang-an.jpg",
+        },
+    ]);
+    const [selectedItemId, setSelectedItemId] = useState(null);
+    const handleDelete = (id) => {
+        // Lọc ra item cần xóa dựa trên id và cập nhật lại trạng thái của giỏ hàng
+        const updatedCartItems = cartItems.filter((item) => item.id !== id);
+        setCartItems(updatedCartItems);
+        if (selectedItemId === id) {
+            setSelectedItemId(null); // Nếu item được xóa đã được chọn, bỏ chọn nó
+        }
+    };
 
-  // Thêm sản phẩm vào giỏ hàng
-  const addToCart = (product) => {
-    const existingItemIndex = cartItems.findIndex(
-      (item) => item.id === product.id
+    // Tính tổng tiền thanh toán
+    const calculateTotal = () => {
+        let total = 0;
+        cartItems.forEach((item) => {
+            const price = parseFloat(item.price.replace(/,/g, ""));
+            total += price * item.quantity;
+        });
+        return total.toLocaleString("vi-VN");
+    };
+    // Tính tổng tiền thanh toán cho các CartItem đã được chọn
+    const calculateTourSelected = () => {
+        let total = 0;
+        cartItems.forEach((item) => {
+            if (item.id === selectedItemId) {
+                const price = parseFloat(item.price.replace(/,/g, ""));
+                total += price * item.quantity;
+            }
+        });
+        return total.toLocaleString("vi-VN");
+    };
+    const handleQuantityChange = (itemId, quantity) => {
+        const updatedCartItems = cartItems.map((item) => {
+            if (item.id === itemId) {
+                return { ...item, quantity: quantity };
+            }
+            return item;
+        });
+        setCartItems(updatedCartItems);
+    };
+    const { setSelectedTour } = useSelectedTour();
+    const handleSelection = (itemId) => {
+        setSelectedItemId(itemId);
+        const selectedTour = cartItems.find((item) => item.id === itemId);
+        setSelectedTour(selectedTour);
+    };
+    return (
+        <>
+            <Navbar/>
+            <div className="container">
+                <div className="container mt-5 p-3 rounded cart">
+                    <div className="row no-gutters">
+                        <div className="col-md-8">
+                            <div className="product-details mr-2">
+                                <div className="d-flex flex-row align-items-center">
+                                    <i className="fas fa-arrow-left"></i>
+                                    <span className="ml-2">Tiếp tục thêm</span>
+                                </div>
+                                <hr/>
+                                <h4 className="mb-0 mb-2">Shopping cart</h4>
+                                <div className="overflow-auto" style={{maxHeight: "400px"}}>
+                                    {cartItems.map((item) => (
+                                        <CartItem
+                                            item={item}
+                                            handleDelete={handleDelete}
+                                            handleQuantityChange={handleQuantityChange}
+                                            selectedItemId={selectedItemId} // Truyền selectedItemId cho CartItem
+                                            handleSelection={handleSelection}
+                                            key={item.id} // Thêm key prop để tránh cảnh báo
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="payment-info">
+                                <div className="d-flex justify-content-between information">
+                                    <span>Tổng tiền giỏ hàng:</span>
+                                    <span>{calculateTotal()}đ</span>
+                                </div>
+                                <div className="d-flex justify-content-between information">
+                                    <span>Tổng tiền tour được chọn:</span>
+                                    <span>{calculateTourSelected()}đ</span>
+                                </div>
+                                <Link className="text-reset me-3" to={`/checkout?total=${calculateTourSelected()}`}>
+                                    <button className="btn btn-primary btn-block d-flex justify-content-between mt-3"
+                                            type="button">
+                                        <span>{calculateTourSelected()}</span>
+                                        <span>
+                                          Thanh toán <i className="fas fa-arrow-right ml-1"></i>
+                                        </span>
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer/>
+        </>
     );
-    if (existingItemIndex >= 0) {
-      // Nếu sản phẩm đã có trong giỏ hàng, tăng số lượng lên 1
-      const updatedItems = [...cartItems];
-      updatedItems[existingItemIndex].quantity += 1;
-      setCartItems(updatedItems);
-    } else {
-      // Nếu sản phẩm chưa có trong giỏ hàng, thêm mới vào
-      setCartItems([...cartItems, { ...product, quantity: 1 }]);
-    }
-  };
-  // Xóa sản phẩm khỏi giỏ hàng
-  const removeFromCart = (productId) => {
-    const updatedItems = cartItems.filter((item) => item.id !== productId);
-    setCartItems(updatedItems);
-  };
-  const handleDelete = (id) => {
-    // Lọc ra item cần xóa dựa trên id và cập nhật lại trạng thái của giỏ hàng
-    const updatedCartItems = cartItems.filter((item) => item.id !== id);
-    setCartItems(updatedCartItems);
-  };
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <div className="container mt-5 p-3 rounded cart">
-          <div className="row no-gutters">
-            <div className="col-md-8">
-              <div className="product-details mr-2">
-                <div className="d-flex flex-row align-items-center">
-                  <i class="fas fa-arrow-left"></i>
-                  <span className="ml-2">Tiếp tục thêm</span>
-                </div>
-                <hr />
-                <h4 className="mb-0 mb-2">Shopping cart</h4>
-                <div className="overflow-auto" style={{ maxHeight: "400px" }}>
-                  {cartItems.map((item) => (
-                    <CartItem
-                      key={item.id}
-                      item={item}
-                      handleDelete={handleDelete}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="payment-info">
-                <div className="d-flex justify-content-between information">
-                  <span>Tổng tiền</span>
-                  <span>$3000.00</span>
-                </div>
-                <div className="d-flex justify-content-between information">
-                  <span></span>
-                  <span>$20.00</span>
-                </div>
-                <div className="d-flex justify-content-between information">
-                  <span>Total(Incl. taxes)</span>
-                  <span>$3020.00</span>
-                </div>
-                <Link className="text-reset me-3" to={"/checkout"}>
-                  <button
-                    className="btn btn-primary btn-block d-flex justify-content-between mt-3"
-                    type="button"
-                  >
-                    <span>$3020.00</span>
-                    <span>
-                      Checkout<i className="fa fa-long-arrow-right ml-1"></i>
-                    </span>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
-    </>
-  );
 };
