c1254ba54a0afe148d4f991d1c6bb6ba0375520b
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-06-02 23:59:12 +0700

    chỉnh sửa chức năng giỏ hàng , cho giỏ hàng chứa trong context

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -7,32 +7,35 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Cart } from "./pages/Cart";
-import { Checkout } from "./pages/Checkout";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {Cart} from "./pages/Cart";
+import {Checkout} from "./pages/Checkout";
 import Tour from "./pages/Tour";
 import Category from "./pages/Category";
-import { SelectedTourProvider } from "./contexts/SelectedTourContext";
-import "../src/styles/App.css";
+import {SelectedTourProvider} from "./contexts/SelectedTourContext";
+import {CartProvider} from "./contexts/CartContext";
+
 function App() {
-  return (
-    <SelectedTourProvider>
-      <Router>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/productList" element={<ProductList />} />
-          <Route path="/productList/productId:" element={<Product />} />
-          <Route path="/tourList/tourId:" element={<Tour />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/tour" element={<Tour />} />
-          <Route path="/category" element={<Category />} />
-        </Routes>
-      </Router>
-    </SelectedTourProvider>
-  );
+    return (
+        <CartProvider>
+            <SelectedTourProvider>
+                <Router>
+                    <Routes>
+                        <Route index element={<Home/>}/>
+                        <Route path="/productList" element={<ProductList/>}/>
+                        <Route path="/productList/productId:" element={<Product/>}/>
+                        <Route path="/tourList" element={<Tour/>}/>
+                        <Route path="/register" element={<Register/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/cart" element={<Cart/>}/>
+                        <Route path="/checkout" element={<Checkout/>}/>
+                        <Route path="/tour" element={<Tour/>}/>
+                        <Route path="/category" element={<Category/>}/>
+                    </Routes>
+                </Router>
+            </SelectedTourProvider>
+        </CartProvider>
+    );
 }
 
 export default App;
