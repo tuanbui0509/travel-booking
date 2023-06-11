8aec5bb2033cc4754d277289a8fa947f5172e553
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-06-12 00:16:02 +0700

    chỉnh sửa cấu trúc tour trong giỏ hàng ,lưu xóa cập nhật tour trong localStorage đồng bộ với redux

--- airbnb-clone/src/components/CartItem.js
+++ airbnb-clone/src/components/CartItem.js
@@ -1,60 +1,59 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React from "react";
 import {formatPrice} from "../utils/utill";
 
-export const CartItem = ({item, handleDelete, handleQuantityChange, selectedItemId, handleSelection}) => {
-    const isSelected = selectedItemId === item.idCard; // Kiểm tra xem item này có được chọn hay không
+export const CartItem = ({item, handleDelete, selectedItemId, handleSelection}) => {
+    const isSelected = selectedItemId === item.id; // Kiểm tra xem item này có được chọn hay không
     // hàm xử lý tick chọn
     const handleTick = () => {
         if (isSelected) {
             handleSelection(null); // Nếu item đã được chọn, bỏ chọn nó
         } else {
-            handleSelection(item.idCard); // Nếu item chưa được chọn, chọn nó
+            handleSelection(item.id); // Nếu item chưa được chọn, chọn nó
         }
     };
 
 
     return (
-        <div className="d-flex justify-content-between align-items-center mt-3 pr-2 items rounded flex-wrap">
+        <div className="d-flex justify-content-between align-items-center mt-3 p-2 items rounded flex-wrap bg-light">
             <div className="d-flex flex-row flex-1">
-                <img className="rounded image-tour-cart" src={item.image} alt={item.title}/>
-                <div className="ml-2 brief">
-                    <span className="font-weight-bold d-block">{item.title}</span>
-                    <ul style={{whiteSpace: "nowrap"}}>
+                <img className="rounded image-tour-cart" src={item.tour.image} alt={item.tour.title}/>
+                <div className="ml-2">
+                    <span className="font-weight-bold d-block">{item.tour.title}</span>
+                    <ul style={{whiteSpace: "nowrap"}} className="ml-1">
                         <li>
-              <span className="spec">
-                <i className="far fa-clock item-icon"></i>Thời gian: {item.time}
-              </span>
+                          <span className="spec">
+                            <i className="far fa-clock item-icon"></i>Thời gian: {item.tour.time}
+                          </span>
                         </li>
                         <li>
-              <span className="spec">
-                <i className="far fa-calendar-alt item-icon"></i>Ngày khởi hành: {item.date}
-              </span>
+                          <span className="spec">
+                            <i className="far fa-calendar-alt item-icon"></i>Ngày khởi hành: {item.tour.date}
+                          </span>
                         </li>
                         <li>
-              <span className="spec">
-                <i className="fas fa-plane-departure mr-2"></i>Phương tiện: {item.plane}
-              </span>
+                          <span className="spec">
+                            <i className="fas fa-plane-departure item-icon-fas"></i>Phương tiện: {item.tour.plane}
+                          </span>
                         </li>
                         <li>
-              <span className="spec">
-                <i className="fas fa-star mr-2"></i>Chất lượng: {item.start}
-              </span>
+                          <span className="spec">
+                            <i className="fas fa-star item-icon-fas"></i>Chất lượng: {item.tour.start}
+                          </span>
                         </li>
                         <li>
-              <span className="spec">
-                <i className="fas fa-users item-icon-fas"></i>Số hành khách tối đa: {item.quantity}
-              </span>
+                          <span className="spec">
+                            <i className="fas fa-users item-icon-fas"></i>Số hành khách tối đa: {item.tour.quantity}
+                          </span>
                         </li>
                         <li>
-              <span className="spec">
-                <i className="fas fa-user item-icon"></i>Số người lớn: {item.quantityAdult}
-              </span>
+                          <span className="spec">
+                            <i className="fas fa-user item-icon"></i>Số người lớn: {item.quantityAdult}
+                          </span>
                         </li>
                         <li>
-              <span className="spec">
-                <i className="fas fa-child item-icon "></i>Số trẻ em: {item.quantityChild}
-              </span>
+                          <span className="spec">
+                            <i className="fas fa-child item-icon "></i>Số trẻ em: {item.quantityChild}
+                          </span>
                         </li>
                     </ul>
                 </div>
@@ -65,8 +64,8 @@ export const CartItem = ({item, handleDelete, handleQuantityChange, selectedItem
                 ) : (
                     <i className="far fa-check-circle tick-icon" onClick={handleTick}></i>
                 )}
-                <span className="d-block font-weight-bold ml-3">{formatPrice(item.price)}đ/1</span>
-                <button className="btn-sm btn-outline-white ml-2 remove-btn" onClick={() => handleDelete(item.idCard)}>
+                <span className="d-block font-weight-bold ml-3">{formatPrice(item.tour.price)}đ/1</span>
+                <button className="btn-sm btn-outline-light ml-2 remove-btn" onClick={() => handleDelete(item.id)}>
                     <i className="fas fa-trash-alt text-danger fa-lg"></i>
                 </button>
             </div>
--- airbnb-clone/src/pages/Cart.js
+++ airbnb-clone/src/pages/Cart.js
@@ -8,57 +8,37 @@ import {useDispatch, useSelector} from "react-redux";
 import {removeCart, updateCart} from "../redux/slices/CartsSlice";
 import {clearSelectedTour, setSelectedTour} from "../redux/slices/SelectedTourSlice";
 import Swal from "sweetalert2";
-import {formatPrice, RATE_PRICE_OF_CHILD_WITH_ADULT, RATE_QUANTITY_OF_CHILD_WITH_ADULT} from "../utils/utill";
+import {
+    formatPrice,
+    RATE_PRICE_OF_CHILD_WITH_ADULT,
+    RATE_QUANTITY_OF_CHILD_WITH_ADULT,
+} from "../utils/utill";
+import {removeCartTourFromLocal, updateCartTourInLocal} from "../utils/localStorageUtils";
 
 export const Cart = () => {
-    // Hàm xử lý khi người dùng kích chuột vào ô input khi chưa được tick chọn
-    const handleInputFocus = () => {
 
-        if (!selectedTour) {
-            Swal.fire({
-                title: "Thông báo",
-                text: "Vui lòng tick chọn tour để chọn số lượng!",
-                icon: "info",
-                confirmButtonText: "OK",
-            });
-        }
-    };
 
-    // "carts": [
-    //     {
-    //         "id": "",
-    //         "quantity": "",
-    //         "tour": [],
-    //         "total_price": "",
-    //         "status": ""
-    //     }
-    // ]
     // Lấy danh sách các tour trong giỏ hàng từ Redux store
     const cartItems = useSelector((state) => state.carts);
 
     // Lấy thông tin về tour được chọn từ Redux store
     const selectedTour = useSelector((state) => state.selectedTour);
 
+
     const dispatch = useDispatch();
 
     // Trạng thái khỏi tạo của tour được chọn (selectedItemId)
-    const [selectedItemId, setSelectedItemId] = useState(selectedTour ? selectedTour.idCard : null);
+    const [selectedItemId, setSelectedItemId] = useState(selectedTour ? selectedTour.id : null);
 
     // xóa tour khỏi giỏ
     const handleDelete = (id) => {
         dispatch(removeCart(id)); // Gửi action xóa từ Redux store
 
-        const storedItems = JSON.parse(localStorage.getItem("cart")) || [];
-        // Lấy dữ liệu từ localStorage (nếu có) và chuyển đổi từ chuỗi JSON thành mảng
-
-        const updatedItems = storedItems.filter((item) => item.idCard !== id);
-        // Lọc ra các mặt hàng khác với mặt hàng có idCard tương ứng
-
-        localStorage.setItem("cart", JSON.stringify(updatedItems));
-        // Lưu mảng dữ liệu mới vào localStorage
+        // xóa khỏi local
+        removeCartTourFromLocal(id)
 
         // nếu tour này có trong tour đã chọn thì xóa 
-        if(selectedTour && selectedTour.idCard === id){
+        if (selectedTour && selectedTour.id === id) {
             dispatch(clearSelectedTour())
             setQuantityAdult(null);
             setQuantityChild(null);
@@ -69,30 +49,30 @@ export const Cart = () => {
     const calculateTotal = () => {
         let total = 0;
         cartItems.forEach((item) => {
-            if (item.price) {
+            if (item.tour.price) {
                 const quantityChild = item.quantityChild !== null ? item.quantityChild : 0;
-                const priceAdult = parseFloat(item.price);
-                const priceChild = parseFloat(item.price) * RATE_PRICE_OF_CHILD_WITH_ADULT;
+                const priceAdult = parseFloat(item.tour.price);
+                const priceChild = parseFloat(item.tour.price) * RATE_PRICE_OF_CHILD_WITH_ADULT;
                 total += priceAdult * item.quantityAdult + priceChild * quantityChild;
             }
         });
         // trả về định dạng tiền VN
-        return formatPrice(total)
+        return total
     };
 
     // tính tổng tiền tour được chọn
     const calculateTourSelected = () => {
         let total = 0;
         cartItems.forEach((item) => {
-            if (item.idCard === selectedItemId && item.price) {
-                const quantityChild = item.quantityChild !== null ? item.quantityChild : 0;
-                const priceAdult = parseFloat(item.price);
-                const priceChild = parseFloat(item.price) * RATE_PRICE_OF_CHILD_WITH_ADULT;
-                total += priceAdult * item.quantityAdult + priceChild * quantityChild;
+            if (item.id === selectedItemId && item.tour.price) {
+                const qChild = quantityChild !== null ? quantityChild : 0;
+                const priceAdult = parseFloat(item.tour.price);
+                const priceChild = parseFloat(item.tour.price) * RATE_PRICE_OF_CHILD_WITH_ADULT;
+                total += priceAdult * quantityAdult + priceChild * qChild;
             }
         });
         // trả về định dạng tiền VN
-        return formatPrice(total)
+        return total
     };
 
 
@@ -106,14 +86,25 @@ export const Cart = () => {
         return result;
     })
 
-    const maxAdults = selectedItemId !== null ? cartItems.find((item) => item.idCard === selectedItemId)?.quantity - quantityChild : 0;
-    const maxChildren = selectedItemId !== null ? cartItems.find((item) => item.idCard === selectedItemId)?.quantity - quantityAdult: 0;
+    const maxAdults = selectedItemId !== null ? cartItems.find((item) => item.id === selectedItemId)?.tour.quantity - quantityChild : 0;
+    const maxChildren = selectedItemId !== null ? cartItems.find((item) => item.id === selectedItemId)?.tour.quantity - quantityAdult : 0;
 
+    // Hàm xử lý khi người dùng kích chuột vào ô input khi chưa được tick chọn
+    const handleInputFocus = () => {
+        if (!selectedTour) {
+            Swal.fire({
+                title: "Thông báo",
+                text: "Vui lòng tick chọn tour để chọn số lượng!",
+                icon: "info",
+                confirmButtonText: "OK",
+            });
+        }
+    };
     const handleAdultQuantityChange = (e) => {
         const value = parseInt(e.target.value);
         console.log(value, maxAdults);
         // nếu số lượng người lớn mà == null ( mặc định bằng 1) thì reset lại child == null (mặc định bằng 0)
-        if (isNaN(value)) {
+        if (isNaN(value) || !selectedTour) {
             setQuantityAdult(null);
             setQuantityChild(null);
             return;
@@ -128,8 +119,8 @@ export const Cart = () => {
                 confirmButtonText: "OK",
             });
         }
-        // nếu sl người lớn mà nhỏ hơn tỉ lệ quy định
-        // sự chênh lệch giữa sl người lớn và trẻ em (RATE_QUANTITY_OF_CHILD_WITH_ADULT)
+            // nếu sl người lớn mà nhỏ hơn tỉ lệ quy định
+            // sự chênh lệch giữa sl người lớn và trẻ em (RATE_QUANTITY_OF_CHILD_WITH_ADULT)
         // thì set lại = tổng sl người lớn + chênh lệch
         else if (value < quantityChild - RATE_QUANTITY_OF_CHILD_WITH_ADULT) {
             setQuantityChild(quantityAdult + RATE_QUANTITY_OF_CHILD_WITH_ADULT)
@@ -157,20 +148,19 @@ export const Cart = () => {
 
     const handleChildQuantityChange = (e) => {
         const value = parseInt(e.target.value);
-        console.log(value, maxChildren)
-        if (isNaN(value)) {
+        if (isNaN(value) || !selectedTour) {
             setQuantityChild(null)
             return
         }
-        // nếu giá trị nhập nhỏ hơn or bằng mức qui định và giá trị này phải < hơn or =
-            // mức chênh lệch sl người lớn và trẻ em thì lấy giá trị này
+            // nếu giá trị nhập nhỏ hơn or bằng mức qui định và giá trị này phải < hơn or =
+        // mức chênh lệch sl người lớn và trẻ em thì lấy giá trị này
         else if (value <= maxChildren && value <= quantityAdult + RATE_QUANTITY_OF_CHILD_WITH_ADULT) {
             setQuantityChild(value);
         }
-        // nếu lớn hơn mức chênh lệch thì thông báo và set lại giá trị bằng số lượng chỗ còn lại
+            // nếu lớn hơn mức chênh lệch thì thông báo và set lại giá trị bằng số lượng chỗ còn lại
         // hoặc cũng có thể là đúng theo tỉ lệ
         else if (value > quantityAdult + RATE_QUANTITY_OF_CHILD_WITH_ADULT) {
-            setQuantityChild(quantityAdult + RATE_QUANTITY_OF_CHILD_WITH_ADULT> maxChildren?maxChildren:
+            setQuantityChild(quantityAdult + RATE_QUANTITY_OF_CHILD_WITH_ADULT > maxChildren ? maxChildren :
                 quantityAdult + RATE_QUANTITY_OF_CHILD_WITH_ADULT)
             Swal.fire({
                 title: "Thông báo",
@@ -192,17 +182,24 @@ export const Cart = () => {
     };
     // hàm xử lý thay đổi số lượng hành khách
     const handleQuantityChange = (itemId, quantityAdult, quantityChild) => {
-        dispatch(updateCart({idCard: itemId, quantityAdult: quantityAdult, quantityChild: quantityChild}));
+        // cập nhật tour trong cart redux
+        dispatch(updateCart({
+            idCard: itemId,
+            quantityAdult: quantityAdult,
+            quantityChild: quantityChild,
+            total_price: calculateTourSelected()
+        }));
         // cập nhật tour được chọn
         dispatch(clearSelectedTour());
-        dispatch(setSelectedTour(cartItems.find((item) => item.idCard === itemId)));
+        dispatch(setSelectedTour(cartItems.find((item) => item.id === itemId)));
+        updateCartTourInLocal(itemId, quantityAdult, quantityChild,calculateTourSelected())
     };
 
 
     // hàm xử lý tour được chọn để tiến hành thanh toán
     const handleSelection = (itemId) => {
-        const selectedTour = cartItems.find((item) => item.idCard === itemId);
-        // nếu có tour được chọn thì set lại ô input với số lượng có trong tour đó
+        const selectedTour = cartItems.find((item) => item.id === itemId);
+        // nếu có tour được chọn thì set lại ô input với số lượng người lớn và trẻ em trong tour đó
         if (selectedTour) {
             setQuantityChild(selectedTour.quantityChild);
             setQuantityAdult(selectedTour.quantityAdult);
@@ -214,19 +211,9 @@ export const Cart = () => {
         if (itemId === null) {
             dispatch(clearSelectedTour());
         }
+        // chọn lại tour này vào redux
         setSelectedItemId(itemId);
-        dispatch(setSelectedTour(cartItems.find((item) => item.idCard === itemId)
-            // gọi hàm callback khi dispatch hoàn thành
-        //     , () => {
-        //     const selectedTour = cartItems.find((item) => item.idCard === itemId);
-        //     if (selectedTour) {
-        //         setQuantityChild(selectedTour.quantityChild);
-        //         setQuantityAdult(selectedTour.quantityAdult);
-        //     } else {
-        //         setQuantityChild(null);
-        //         setQuantityAdult(null);
-        //     }
-        // }
+        dispatch(setSelectedTour(cartItems.find((item) => item.id === itemId)
         ));
     };
 
@@ -255,17 +242,16 @@ export const Cart = () => {
                     <div className="row no-gutters">
                         <div className="col-md-8">
                             <div className="product-details mr-2">
-                                <h4 className="mb-0 mb-2">Shopping cart</h4>
+                                <h4 className="mb-0 mb-2">Tour cart</h4>
                                 <div className="overflow-auto overflow-cart">
                                     {cartItems.length > 0 ? (
                                         cartItems.map((item) => (
                                             <CartItem
                                                 item={item}
                                                 handleDelete={handleDelete}
-                                                handleQuantityChange={handleQuantityChange}
                                                 selectedItemId={selectedItemId}
                                                 handleSelection={handleSelection}
-                                                key={item.idCard}
+                                                key={item.id}
                                             />
                                         ))
                                     ) : (
@@ -281,7 +267,7 @@ export const Cart = () => {
                                         type="number"
                                         className="form-control"
                                         id="name"
-                                        placeholder={`${selectedTour !== null ? cartItems.find((item) => item.idCard === selectedTour.idCard)?.price : ""} x số người lớn`}
+                                        placeholder={`${selectedTour !== null ? cartItems.find((item) => item.id === selectedTour.id)?.tour.price : ""} x số người lớn`}
                                         inputMode="numeric"
                                         min="1"
                                         max={maxAdults}
@@ -296,7 +282,7 @@ export const Cart = () => {
                                         className="form-control"
                                         id="name"
                                         inputMode="numeric"
-                                        placeholder={`${selectedTour !== null ? cartItems.find((item) => item.idCard === selectedTour.idCard)?.price * RATE_PRICE_OF_CHILD_WITH_ADULT : ""} x số trẻ em dưới 7 tuổi`}
+                                        placeholder={`${selectedTour !== null ? cartItems.find((item) => item.id === selectedTour.id)?.tour.price * RATE_PRICE_OF_CHILD_WITH_ADULT : ""} x số trẻ em dưới 7 tuổi`}
                                         min="0"
                                         max={maxChildren}
                                         value={quantityChild === null ? "" : quantityChild}
@@ -307,30 +293,30 @@ export const Cart = () => {
 
                                 <div className="d-flex justify-content-between information">
                                     <span>Tổng tiền giỏ hàng:</span>
-                                    <span>{calculateTotal()}đ</span>
+                                    <span>{formatPrice(calculateTotal())}đ</span>
                                 </div>
                                 {
                                     selectedTour ? (<>
-                                <div className="d-flex justify-content-between information">
-                                    <span>Giá vé cho người lớn:</span>
-                                    <span>{formatPrice(selectedTour.price)}đ</span>
-                                </div>
-                                <div className="d-flex justify-content-between information">
-                                    <span>Giá vé cho trẻ em:</span>
-                                    <span>{formatPrice(selectedTour.price * RATE_PRICE_OF_CHILD_WITH_ADULT)}đ</span>
-                                </div>
-                                <div className="d-flex justify-content-between information">
-                                    <span>Tổng tiền tour được chọn:</span>
-                                    <span>{calculateTourSelected()}đ</span>
-                                </div>
+                                            <div className="d-flex justify-content-between information">
+                                                <span>Giá vé cho người lớn:</span>
+                                                <span>{formatPrice(selectedTour.tour.price)}đ</span>
+                                            </div>
+                                            <div className="d-flex justify-content-between information">
+                                                <span>Giá vé cho trẻ em:</span>
+                                                <span>{formatPrice(selectedTour.tour.price * RATE_PRICE_OF_CHILD_WITH_ADULT)}đ</span>
+                                            </div>
+                                            <div className="d-flex justify-content-between information">
+                                                <span>Tổng tiền tour được chọn:</span>
+                                                <span>{formatPrice(calculateTourSelected())}đ</span>
+                                            </div>
                                         </>)
-                                    : ""
+                                        : ""
                                 }
                                 <Link className="text-reset me-3" to={`/checkout?total=${calculateTourSelected()}`}>
                                     <button
                                         className="btn btn-amber btn-block d-flex justify-content-between mt-3 rounded"
                                         type="button">
-                                        <span>{calculateTourSelected()}đ</span>
+                                        <span>{formatPrice(calculateTourSelected())}đ</span>
                                         <span>
                                           Thanh toán <i className="fas fa-arrow-right ml-1"></i>
                                         </span>
