6f39de64b4d62af211f98ef0cbf978340fe6723c
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-06-16 14:26:29 +0700

    chức năng chọn phương thức thanh toán

--- airbnb-clone/src/redux/slices/CheckoutSlice.js
+++ airbnb-clone/src/redux/slices/CheckoutSlice.js
@@ -1,4 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {addCheckoutToLocal, removeCartTourFromLocal} from "../../utils/localStorageUtils";
+import {clearSelectedTour} from "./SelectedTourSlice";
+import {removeCart} from "./CartsSlice";
 
 const checkoutSlice = createSlice({
     name: "checkout",
@@ -12,11 +15,23 @@ const checkoutSlice = createSlice({
                 total_price: action.payload.total_price,
                 payment_method: action.payload.payment_method,
                 status: "pending",
-                passenger_details:action.payload.passenger_details
+                passenger_details: action.payload.passenger_details
             }
             state.push(newCheckout)
-        }
+        },
+        updatePayment: (state, action) => {
+            return state.map((item) => {
+                if (item.id === action.payload.id) {
+                    return {
+                        ...item,
+                        payment_method: action.payload.payment_method
+                    }
+                }
+                return item;
+            })
+        },
+
     },
 })
-export const { saveInfoPassenger } = checkoutSlice.actions;
+export const {saveInfoPassenger, updatePayment} = checkoutSlice.actions;
 export default checkoutSlice.reducer;
