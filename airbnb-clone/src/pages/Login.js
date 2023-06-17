71434ae0f6fbb6b13c675fe355fb5a24900a1e9d
linhnt <20130307@st.hcmuaf.edu.vn>
2023-06-17 13:21:34 +0700

    commit pull

--- airbnb-clone/src/pages/Login.js
+++ airbnb-clone/src/pages/Login.js
@@ -1,83 +1,35 @@
-import styled from "styled-components";
-import {mobile} from "../responsive";
-import { Link } from  "react-router-dom"
+import React, {useState} from "react";
+import Navbar from "../components/Navbar";
+import "../styles/login.scss";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-    const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(
-        rgba(255, 255, 255, 0.5),
-        rgba(255, 255, 255, 0.5)
-      ),
-      url("https://images.pexels.com/photos/1450353/pexels-photo-1450353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
-        center;
-        background-size: cover;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-    const Wrapper = styled.div`
-    width: 40%;
-    padding: 50px;
-    background-color: white;
-    border-radius: 2.5%;  
-    display: flex;
-    flex-direction: column;
-    ${mobile({ width: "70%" })};
-`
-    const Title = styled.div`
-    font-size: 36px;
-    text-align: center;  
-    font-weight: 400;
-`
-    const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-`
-    const Input = styled.input`
-    flex: 1;
-    min-width: 40%;
-    margin: 10px 0px;
-    padding: 10px;
-`
-
-    const Button = styled.button`
-      width: 100%;
-      border: none;
-      padding: 15px 20px;
-      background-color: #006680;
-      color: white;
-      cursor: pointer;
-      margin-bottom: 10px;
-    `
-    const LinkText = styled.a`
-    margin: 5px 0px;
-    font-size: 18px;
-    text-decoration: underline;
-    cursor: pointer;
-`
-
-    const WrapperLink = styled.div`
-    display: flex;
-    justify-content: space-between;`
-
-    const Login = () => {
-        return (
-            <Container>
-                <Wrapper>
-                    <Title>Đăng nhập</Title>
-                    <Form>
-                        <Input placeholder="Username"/>
-                        <Input placeholder="Mật khẩu"/>
-                        <Button>ĐĂNG NHẬP</Button>
+export const Login = () => {
+    return (
+        <>
+            <title>Đăng nhập</title>
+            <Navbar/>
+            <div className="container2">
+                <div className="wrapper">
+                    <div className="title">Tạo tài khoản</div>
+                    <Form className="formLogin">
+                        <Form.Group className="inputValue" controlId="formBasicEmail">
+                            <Form.Control type="email" name='email' placeholder="Email"/>
+                        </Form.Group>
+                        <Form.Group className="inputValue" controlId="formBasicEmail">
+                            <Form.Control type="password" name='password' placeholder="Mật khẩu"/>
+                        </Form.Group>
+                        <Button variant="primary" className='buttonLogin' type="submit">
+                            Đăng nhập
+                        </Button>
                     </Form>
-                    <WrapperLink>
-                        <Link to={"/forgetPassword"}><LinkText>Quên mật khẩu?</LinkText></Link>
-                        <Link to={"/register"}><LinkText>Tạo tài khoản mới</LinkText></Link>
-                    </WrapperLink>
-
-                </Wrapper>
-            </Container>
-        )
+                    <div className="linktext">
+                        <a href="/login" className="link">Quên mật khẩu</a>
+                        <a href="/register" className="link">Tạo tài khoản mới</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
-export default Login;
+
--- airbnb-clone/src/pages/Register.js
+++ airbnb-clone/src/pages/Register.js
@@ -1,85 +1,180 @@
-import styled from "styled-components";
-import { mobile } from "../responsive";
+import React, {useState} from "react";
+import {ToastContainer, toast} from 'react-toastify';
+import {Link, useNavigate} from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import "../styles/register.scss";
+import Navbar from "../components/Navbar";
+export const Register = () => {
 
-const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(
-        rgba(255, 255, 255, 0.5),
-        rgba(255, 255, 255, 0.5)
-      ),
-      url("https://images.pexels.com/photos/1450353/pexels-photo-1450353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
-        center;
-        background-size: cover;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-const Wrapper = styled.div`
-    width: 40%;
-    padding: 20px;
-    border-radius: 2.5%;
-    background-color: white;
-    ${mobile({ width: "70%" })};
-`
-const Title = styled.div`
-    font-size: 24px;
-    font-weight: 300;
-`
-const Form = styled.form`
-    display: flex;
-    flex-wrap: wrap;
-`
-const Input = styled.input`
-    flex: 1;
-    min-width: 40%;
-    margin: 20px 10px 0px 0px;
-    padding: 10px;
-`
-const Agreement = styled.span`
-    font-size: 14px;
-    margin: 20px 0px;
-    text-align: center;
-`
+    const [username, usernamechange] = useState("");
+    const [fullname, fullnamechange] = useState("");
+    const [email, emailchange] = useState("");
+    const [phone, phonechange] = useState("");
+    const [password, passwordchange] = useState("");
+    const [repeatPassword, repeatPasswordchange] = useState("");
 
-const Span = styled.span`
-    font-size: 14px;
-    color: #006680;
-  justify-content: space-between;
-  
-`
-const Button = styled.button`
-    width: 100%;
-    border: none;
-    padding: 15px 20px;
-    background-color: #006680;
-    color: white;
-    cursor: pointer;
-`
+    const history = useNavigate();
+    const [showAlert, setAlert] = useState(false);
 
+    const isValidate =() => {
+        let isprocceed = true;
+        let errmessage = '';
+        if (username === null || username ==='') {
+            isprocceed = false;
+            errmessage = 'Vui lòng nhập username';
+        }
+        if (fullname === null || fullname ==='') {
+            isprocceed = false;
+            errmessage = 'Vui lòng nhập Họ tên';
+        }
+        if (email === null || email ==='') {
+            isprocceed = false;
+            errmessage = 'Vui lòng nhập email';
+        }
+        if (!email.includes("@")) {
+            isprocceed = false;
+            errmessage = 'Email không hợp lệ';
+        }
+        if (phone.length != 10) {
+            isprocceed = false;
+            errmessage = 'Số điện thoại phải đủ 10 số';
+        }
+        if (password.length < 4) {
+            isprocceed = false;
+            errmessage = 'Độ dài mật khẩu phải lớn hơn 4';
+        }
+        if (password !== repeatPassword) {
+            isprocceed = false;
+            errmessage = 'Mật khẩu không trùng khớp';
+        }
+        return isprocceed;
+    }
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        let regisObj = { username, fullname, email, phone, password, repeatPassword};
+        // console.log(regisObj);
 
-const Login = () => {
+        if (isValidate()) {
+            fetch("http://localhost:5000/api/users", {
+                method: "POST",
+                headers: {'content-type': 'application/json'},
+                body: JSON.stringify(regisObj)
+            }).then((res ) => {
+                history('/login')
+                localStorage.setItem("users",JSON.stringify(regisObj));
+            }).catch((err) => {
+                toast.error( err.message + 'failed');
+            });
+        }
+    }
+
+    // const addData = (e) => {
+    //     e.preventDefault();
+    //
+    //     const {username, fullname, email, phone, password, repeatPassword} = inpval;
+    //     if (username === "") {
+    //         toast.error('Vui lòng nhập username', {
+    //             position: "top-center",
+    //         });
+    //     } else if (fullname === "") {
+    //         toast.error('Vui lòng nhập Họ tên', {
+    //             position: "top-center",
+    //         });
+    //     } else if (email === "") {
+    //         toast.error('Vui lòng nhập email', {
+    //             position: "top-center",
+    //         })
+    //     } else if (!email.includes("@")) {
+    //         toast.error('Email không hợp lệ', {
+    //             position: "top-center",
+    //         })
+    //     } else if (phone.length != 10) {
+    //         toast.error('Số điện thoại không hợp lệ', {
+    //             position: "top-center",
+    //         })
+    //     } else if (password.length < 7) {
+    //         toast.error('Độ dài mật khẩu phải lớn hơn 7', {
+    //             position: "top-center",
+    //         })
+    //     } else if (repeatPassword.length < 7) {
+    //         toast.error('Độ dài mật khẩu phải lớn hơn 7', {
+    //             position: "top-center",
+    //         })
+    //     } else if (password !== repeatPassword) {
+    //         toast.error('Mật khẩu không trùng khớp')
+    //     }
+    //     else {
+    //         console.log("Đăng ký thành công");
+    //         history("/login")
+    //         localStorage.setItem("user",JSON.stringify([...data,inpval]));
+    //         fetch("http://localhost:5000/users", {
+    //             method: "POST",
+    //             headers: {'content-type': 'application/json'},
+    //             body: JSON.stringify(inpval)
+    //         }).then((res) => {
+    //             toast.success('Đăng ký thành công')
+    //             history('/login')
+    //         }).catch((err) => {
+    //             toast.error('Vui lòng nhập lại thông tin');
+    //         })
+    //     }
+    //
+    //     // if (getdata(e)) {
+    //     //     fetch("http://localhost:5000/users", {
+    //     //         method: "POST",
+    //     //         headers: {'content-type': 'application/json'},
+    //     //         body: JSON.stringify(inpval)
+    //     //     }).then((res) => {
+    //     //         toast.success('Đăng ký thành công')
+    //     //         history('/login')
+    //     //         localStorage.setItem("user",JSON.stringify([...data,inpval]));
+    //     //     }).catch((err) => {
+    //     //         toast.error('Vui lòng nhập lại thông tin');
+    //     //     })
+    //     // }
+    // }
     return (
-        <Container>
-            <Wrapper>
-                <Title>Tạo tài khoản</Title>
-                <Form>
-                    <Input placeholder="Username"/>
-                    <Input placeholder="Họ tên"/>
-                    <Input placeholder="Email"/>
-                    <Input placeholder="Số điện thoại"/>
-                    <Input placeholder="Mật khẩu"/>
-                    <Input placeholder="Nhập lại mật khẩu"/>
-                    <Agreement>
-                        Bằng việc đăng ký, bạn đã đồng ý với XYZ về
-                            <Span> Điều khoản sử dụng  </Span> và
-                            <Span> Chính sách bảo mật</Span>
-                    </Agreement>
-                    <Button>Tạo</Button>
-                </Form>
-            </Wrapper>
-        </Container>
+        <>
+            <title>Tạo tài khoản</title>
+            <Navbar/>
+            <div className="container1" onSubmit={handlesubmit}>
+                <div className="wrapper">
+                    <div className="title">Tạo tài khoản</div>
+                    <Form className="formRegister">
+                        <Form.Group className="inputValue" controlId="formBasicEmail">
+                            <Form.Control value={username} type="text" name='username' onChange={e=>usernamechange(e.target.value)} placeholder="Username"/>
+                        </Form.Group>
+                        <Form.Group className="inputValue" controlId="formBasicEmail">
+                            <Form.Control value={fullname} type="text" name='fullname' onChange={e=>fullnamechange(e.target.value)} placeholder="Họ tên"/>
+                        </Form.Group>
+                        <Form.Group className="inputValue" controlId="formBasicEmail">
+                            <Form.Control value={email} type="email" name='email' onChange={e=>emailchange(e.target.value)}  placeholder="Email"/>
+                        </Form.Group>
+                        <Form.Group className="inputValue" controlId="formBasicEmail">
+                            <Form.Control value={phone} type="phone" name='phone' onChange={e=>phonechange(e.target.value)} placeholder="Số điện thoại"/>
+                        </Form.Group>
+                        <Form.Group className="inputValue" controlId="formBasicPassword">
+                            <Form.Control value={password} type="password" name='password' onChange={e=>passwordchange(e.target.value)} placeholder="Mật khẩu"/>
+                        </Form.Group>
+                        <Form.Group className="inputValue" controlId="formBasicPassword">
+                            <Form.Control value={repeatPassword} type="password" name='repeatPassword' onChange={e=>repeatPasswordchange(e.target.value)}
+                                          placeholder="Nhập lại mật khẩu"/>
+                        </Form.Group>
+                        <span className="agreement">
+                            Bằng việc đăng ký, bạn đã đồng ý với XYZ về
+                            <span className="linkAgree"> Điều khoản sử dụng  </span> và
+                            <span className="linkAgree"> Chính sách bảo mật</span>
+                        </span>
+                        <Button variant="primary" className='buttonRegis'  type="submit">
+                            Tạo tài khoản
+                        </Button>
+                    </Form>
+                    <p className="mt-3" style={{textAlign: "center"}}> Bạn đã có tài khoản ?
+                        <span> <Link to = "/login">Đăng nhập</Link></span>
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
-
-export default Login
